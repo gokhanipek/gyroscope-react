6e5da1ff4ef648ffd555605dfbc0c12394d69ad8
gokhanipek <gipek03@gmail.com>
2024-01-12 00:00:50 +0100

    gyroscope check

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,17 @@
-import { useState } from 'react';
 import './App.css'
-import { useEffect } from 'react';
+import Tilt from "react-parallax-tilt";
+
 
 function App() {
-
-  const [gyroscopeData, setGyroscopeData] = useState({
-    alpha: null,
-    beta: null,
-    gamma: null,
-  });
-
-  const [status, setStatus] = useState('');
-
-  useEffect(() => {
-    const handleOrientation = (event) => {
-      setGyroscopeData({
-        alpha: event.alpha,
-        beta: event.beta,
-        gamma: event.gamma,
-      });
-    };
-
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener('deviceorientation', handleOrientation);
-      setStatus("As device is allowing this should");
-    } else {
-      console.log('Gyroscope not supported on this device.');
-      setStatus("As device is not allowing, this should not");
-    }
-
-    return () => {
-      window.removeEventListener('deviceorientation', handleOrientation);
-    };
-  }, []);
-
-
   return (
-    <div className='gyroscope'>{status} move on phone movement <br /> {gyroscopeData.alpha} - {gyroscopeData.beta} - {gyroscopeData.gamma}</div>
+    <Tilt glareEnable={true} tiltMaxAngleX={10}
+      tiltMaxAngleY={10} perspective={1000}
+      glareColor={"rgb(255,0,0)"}>
+      <div className='tiltComponent'>
+        Move biatch
+      </div>
+    </Tilt>
+
   )
 }
 
