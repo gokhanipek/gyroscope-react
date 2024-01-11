13904af94b18a3b0aae6b335892bb0cc968208e9
gokhanipek <gipek03@gmail.com>
2024-01-11 23:42:32 +0100

    gyroscope check

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [gyroscopeData, setGyroscopeData] = useState({
+    alpha: null,
+    beta: null,
+    gamma: null,
+  });
+
+  const [status, setStatus] = useState('');
+
+  useEffect(() => {
+    const handleOrientation = (event) => {
+      setGyroscopeData({
+        alpha: event.alpha,
+        beta: event.beta,
+        gamma: event.gamma,
+      });
+    };
+
+    if (window.DeviceOrientationEvent) {
+      window.addEventListener('deviceorientation', handleOrientation);
+      setStatus("As device is allowing this should");
+    } else {
+      console.log('Gyroscope not supported on this device.');
+      setStatus("As device is not allowing, this should not");
+    }
+
+    return () => {
+      window.removeEventListener('deviceorientation', handleOrientation);
+    };
+  }, []);
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='gyroscope'>{status} move on phone movement <br /> {gyroscopeData.alpha} - {gyroscopeData.beta} - {gyroscopeData.gamma}</div>
   )
 }
 
-export default App
+export default App;
